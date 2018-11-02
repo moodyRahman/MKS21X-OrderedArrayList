f6c3f5e06c8fab7b324c3139626b629369d397fa
Moody <mrahman01@stuy.edu>
2018-11-02 16:16:01 -0400

    added a proper add(T element); [untested]

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,6 +8,12 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 	}
 
 	public void add(T element){
-		super.add(element);
+		int temp;
+		for (int x = 0; x < this.size(); x++){
+			if (this.get(x).compareTo(element)>0){
+				temp = x;
+			}
+		}
+		super.add(temp, element);
 	}
 }
