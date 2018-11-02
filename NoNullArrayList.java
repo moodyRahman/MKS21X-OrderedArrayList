4c820a4907ca08c19906d56ca26459123fc3735d
Moody <mrahman01@stuy.edu>
2018-11-02 16:13:41 -0400

    added add(int, String)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -14,6 +14,13 @@ public class NoNullArrayList<T> extends ArrayList<T>{
 		super.add(element);
 	}
 
+	public void add(int index, String element){
+		if (element == null){
+			throw new IllegalArgumentException();
+		}
+		super.add(index, element);
+	}
+
 	public void set(int index, T element){
 		if (element == null){
 			throw new IllegalArgumentException();
