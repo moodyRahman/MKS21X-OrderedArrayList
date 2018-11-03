587eeeca999784c8db9fd61ed85203d2687cf5a0
Moody <mrahman01@stuy.edu>
2018-11-03 12:50:07 -0400

    .

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -18,26 +18,3 @@ public class NoNullArrayList<T> extends ArrayList<T>{
 	    }
 	  }
 	}
-	/**
-	public void add(T element){
-		if (element == null){
-			throw new IllegalArgumentException();
-		}
-		super.add(element);
-	}
-	public void add(int index, T element){
-		if (element == null){
-			throw new IllegalArgumentException();
-		}
-		super.add(index, element);
-	}
-
-public int set(int index, T element){
-		if (element == null){
-			throw new IllegalArgumentException();
-		}
-		super.set(index, element);
-		return get(index);
-	}
-}
-**/
