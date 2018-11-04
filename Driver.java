6cc29e554791c4e4eb5edf62f3da7f681a23aae5
Moody <mrahman01@stuy.edu>
2018-11-03 20:10:35 -0400

    test cases for OrderedArray [not working]

--- Driver.java
+++ Driver.java
@@ -3,9 +3,41 @@ public class Driver {
 	public static void main(String args[]){
 		NoNullArrayList<String> list=new NoNullArrayList<String>();//Creating arraylist
 		list.add("Ravi");
-		list.add("Vijay");
+		list.add(1, "Vijay");
 		list.add("Ravi");
 		list.add("Ajay");
+		list.add(0, "Dejay");
+		list.add("Devon");
 		System.out.println(list);
+		try {
+			list.add(null);
+		}catch (IllegalArgumentException e){
+			System.out.println(".add(null) error caught!");
+		}
+		System.out.println(list);
+		try {
+			list.add(4, null);
+		}catch (IllegalArgumentException e){
+			System.out.println(".add(4, null) error caught!");
+		}
+		try {
+			list.add(99, "aaaa");
+		}catch (IndexOutOfBoundsException e){
+			System.out.println("index error caught!");
+		}
+
+		System.out.println();
+
+		System.out.println("--------");
+
+		OrderedArrayList<String> olist = new OrderedArrayList<String>();
+		olist.add("aware");
+		olist.add("slit");
+		olist.add("float");
+		olist.add("lettuce");
+		olist.add("ossified");
+		olist.add("icy");
+
+		System.out.println(olist);
 	}
 }
