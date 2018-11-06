8f2ce2ca22df27e32b21cdbf7949b0226300beef
Moody <mrahman01@stuy.edu>
2018-11-05 22:27:45 -0500

    added set(int, T)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -31,4 +31,11 @@ public class NoNullArrayList<T> extends ArrayList<T>{
 
 		return true;
 	}
+
+	public T set(int index, T element){
+		if (element == null){
+			throw new IllegalArgumentException();
+		}
+		return super.set(index, element);
+	}
   }
